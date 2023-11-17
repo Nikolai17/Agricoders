dc69df68d8384c49ecb3e9f4526097f0d3bc84c7
Nikolay Volnikov <nvolnikov@rencredit.ru>
2023-11-17 20:49:22 +0300

    add files

---
+++ Agricoders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Agricoders
+//
+//  Created by Nikolay Volnikov on 17.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
